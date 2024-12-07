7260ea576dadd2a89a93c707926e5ad00f616c25
tonytangdev <tony.tang.developer@gmail.com>
2024-12-07 14:40:59 +0100

    test: enhance Pharmacy tests for Fervex behavior

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -128,4 +128,84 @@ describe("Pharmacy", () => {
       expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([drug]);
     });
   });
+
+  describe("Fervex", () => {
+    it("should increase the benefit by 1 when expiresIn is greater than 10", () => {
+      const expiresIn = faker.number.int({ min: 11, max: 99 });
+      const benefit = faker.number.int({
+        min: MIN_BENEFIT,
+        max: MAX_BENEFIT - 1,
+      });
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, benefit + 1);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+
+    it("should increase the benefit by 2 when expiresIn is between 6 and 10", () => {
+      const expiresIn = faker.number.int({ min: 6, max: 10 });
+      const benefit = faker.number.int({
+        min: MIN_BENEFIT,
+        max: MAX_BENEFIT - 2,
+      });
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, benefit + 2);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+
+    it("should increase the benefit by 3 when expiresIn is between 1 and 5", () => {
+      const expiresIn = faker.number.int({ min: 1, max: 5 });
+      const benefit = faker.number.int({
+        min: MIN_BENEFIT,
+        max: MAX_BENEFIT - 3,
+      });
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, benefit + 3);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+
+    it(`should set the benefit to ${MIN_BENEFIT} when expiresIn is 0 or less`, () => {
+      const expiresIn = faker.number.int({ min: -99, max: 0 });
+      const benefit = faker.number.int({
+        min: MIN_BENEFIT,
+        max: MAX_BENEFIT - 3,
+      });
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, 0);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+
+    it("should increase the benefit by 3 when expiresIn becomes 0", () => {
+      const expiresIn = 1;
+      const benefit = faker.number.int({
+        min: MIN_BENEFIT,
+        max: MAX_BENEFIT - 3,
+      });
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, benefit + 3);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+
+    it(`should not increase the benefit above ${MAX_BENEFIT} when it should increase by 2`, () => {
+      const expiresIn = faker.number.int({ min: 6, max: 10 });
+      const benefit = MAX_BENEFIT - 1;
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, MAX_BENEFIT);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+
+    it(`should not increase the benefit above ${MAX_BENEFIT} when it should increase by 3`, () => {
+      const expiresIn = faker.number.int({ min: 1, max: 5 });
+      const benefit = MAX_BENEFIT - 1;
+      const drug = new Drug("Fervex", expiresIn, benefit);
+
+      const expectedDrug = new Drug("Fervex", expiresIn - 1, MAX_BENEFIT);
+      expect(new Pharmacy([drug]).updateBenefitValue()).toEqual([expectedDrug]);
+    });
+  });
 });
